9f28d1a510b5a93621d127b64a71d54754edc662
marvin <marvkipi@gmail.com>
2015-12-20 22:47:40 -0800

    login facebook --data_stream failed

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,10 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngOpenFB'])
 
-.run(function($ionicPlatform) {
+.run(function ($ionicPlatform, ngFB) {
+  ngFB.init({appId: '1663145390627348'});
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -22,7 +23,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
 
     .state('app', {
